04a032d6c0dbcd460fdc7465a8076fa65d83b9dc
JuniorPoop <129127747+JuniorPoop@users.noreply.github.com>
2024-03-09 00:07:59 -0500

    Update lab_assignment_7.c

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -39,56 +39,56 @@ int main() {
     int array1[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
     int array2[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
     int n = sizeof(array1) / sizeof(array1[0]);
-    struct Element elements1_bubble[n];
-    struct Element elements1_selection[n];
-    struct Element elements2_bubble[n];
-    struct Element elements2_selection[n];
-  
+    struct Element bubble1[n];
+    struct Element selection1[n];
+    struct Element bubble2[n];
+    struct Element selection2[n];
+
     for (int i = 0; i < n; i++) {
-        elements1_bubble[i].value = array1[i];
-        elements1_bubble[i].swaps = 0;
+        bubble1[i].value = array1[i];
+        bubble1[i].swaps = 0;
 
-        elements1_selection[i].value = array1[i];
-        elements1_selection[i].swaps = 0;
+        selection1[i].value = array1[i];
+        selection1[i].swaps = 0;
 
-        elements2_bubble[i].value = array2[i];
-        elements2_bubble[i].swaps = 0;
+        bubble2[i].value = array2[i];
+        bubble2[i].swaps = 0;
 
-        elements2_selection[i].value = array2[i];
-        elements2_selection[i].swaps = 0;
+        selection2[i].value = array2[i];
+        selection2[i].swaps = 0;
     }
-    bubbleSort(elements1_bubble, n);
+    bubbleSort(bubble1, n);
     printf("array1 Bubble Sort:\n");
     for (int i = 0; i < n; i++) {
-        printf("%d: %d \n", elements1_bubble[i].value, elements1_bubble[i].swaps);
+        printf("%d: %d \n", bubble1[i].value, bubble1[i].swaps);
     }
-    int totalSwaps1_bubble = 0;
+    int totalSwaps1 = 0;
     for (int i = 0; i < n; i++) {
-        totalSwaps1_bubble += elements1_bubble[i].swaps;
+        totalSwaps1 += bubble1[i].swaps;
     }
-    printf("%d\n\n", totalSwaps1_bubble/2);
-    bubbleSort(elements2_bubble, n);
+    printf("%d\n\n", totalSwaps1/2);
+    bubbleSort(bubble2, n);
     printf("array2 Bubble Sort:\n");
     for (int i = 0; i < n; i++) {
-        printf("%d: %d \n", elements2_bubble[i].value, elements2_bubble[i].swaps);
+        printf("%d: %d \n", bubble2[i].value, bubble2[i].swaps);
     }
-    int totalSwaps2_bubble = 0;
+    int totalSwaps2 = 0;
     for (int i = 0; i < n; i++) {
-        totalSwaps2_bubble += elements2_bubble[i].swaps;
+        totalSwaps2 += bubble2[i].swaps;
     }
-    printf("%d\n\n", totalSwaps2_bubble/2);
-    selectionSort(elements1_selection, n);
+    printf("%d\n\n", totalSwaps2/2);
+    selectionSort(selection1, n);
     printf("array1 Selection Sort:\n");
     for (int i = 0; i < n; i++) {
-        printf("%d: %d \n", elements1_selection[i].value, elements1_selection[i].swaps);
+        printf("%d: %d \n", selection1[i].value, selection1[i].swaps);
     }
     int totalSwaps1_selection = n - 1; 
     printf("%d\n\n", totalSwaps1_selection);
 
-    selectionSort(elements2_selection, n);
+    selectionSort(selection2, n);
     printf("array2 Selection Sort:\n");
     for (int i = 0; i < n; i++) {
-        printf("%d: %d \n", elements2_selection[i].value, elements2_selection[i].swaps);
+        printf("%d: %d \n", selection2[i].value, selection2[i].swaps);
     }
     int totalSwaps2_selection = n - 1;
     printf("%d\n\n", totalSwaps2_selection/2);
@@ -96,3 +96,4 @@ int main() {
 
     return 0;
 }
+
